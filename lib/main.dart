59aa97a0bdf73d0a7093a88e2198a2e2f0fdab9e
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-12-20 17:26:15 +0530

    added stateless wiget for hotreload

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("My First App"),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: const Center(
-          child: Text(
-              "Hello Developers",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey,
-                fontFamily: "IndieFlower",
-              ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () { },
-          backgroundColor: Colors.red[600],
-          child: const Text('Click'),
-        ),
-      ),
-
+      home: Home(),
   ));
 }
+
+
+class Home extends StatelessWidget {
+  //const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello Developers",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: "IndieFlower",
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        backgroundColor: Colors.red[600],
+        child: const Text('Click'),
+      ),
+    );
+  }
+}
+
 
